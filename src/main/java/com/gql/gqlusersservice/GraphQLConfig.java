89d69262eb2107440a3f227888fd93343735d4e1
Shrinivas Shukla <ssshukla1993@gmail.com>
2022-07-07 01:00:40 +0530

    Added @key & reference resolver testing

--- src/main/java/com/gql/gqlusersservice/GraphQLConfig.java
+++ src/main/java/com/gql/gqlusersservice/GraphQLConfig.java
@@ -1,16 +1,33 @@
 package com.gql.gqlusersservice;
 
 import com.apollographql.federation.graphqljava.Federation;
+import com.apollographql.federation.graphqljava._Entity;
 import graphql.kickstart.tools.SchemaParser;
 import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.SchemaPrinter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 public class GraphQLConfig {
 
     @Bean
     public GraphQLSchema customSchema(SchemaParser schemaParser){
-        return Federation.transform(schemaParser.makeExecutableSchema()).build();
+        return Federation.transform(schemaParser.makeExecutableSchema())
+                .fetchEntities(env -> env.<List<Map<String, Object>>>getArgument(_Entity.argumentName)
+                        .stream()
+                        .map(values -> null)
+                        .collect(Collectors.toList()))
+                .resolveEntityType(env -> null)
+                .build();
+    }
+
+    @Bean
+    public SchemaPrinter customSchemaPrinter(){
+        return new SchemaPrinter(SchemaPrinter.Options.defaultOptions().includeDirectives(true).includeDirectiveDefinitions(true).includeSchemaDefinition(true).includeIntrospectionTypes(true));
     }
 }
